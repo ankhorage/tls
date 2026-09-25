cc20c3eaacf36663c727d5dac18159aa96b23add
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-25 03:22:59 +0200

    refactor: remove Docker-owned TLS storage

--- src/features/certificates/constants/certbot.ts
+++ src/features/certificates/constants/certbot.ts
@@ -1,7 +1,7 @@
 export const CERTBOT_IMAGE = 'certbot/certbot:v5.8.0';
-export const CERTBOT_STORAGE_VOLUME = 'ankhorage-tls';
-const CERTBOT_DATA_ROOT = '/data/certbot';
-export const CERTBOT_WEBROOT = `${CERTBOT_DATA_ROOT}/webroot`;
-export const CERTBOT_CONFIG_DIR = `${CERTBOT_DATA_ROOT}/config`;
-export const CERTBOT_WORK_DIR = `${CERTBOT_DATA_ROOT}/work`;
-export const CERTBOT_LOGS_DIR = `${CERTBOT_DATA_ROOT}/logs`;
+export const CERTBOT_DEFAULT_STORAGE_DIRECTORY = '.ankh-tls';
+export const CERTBOT_CONTAINER_ROOT = '/data';
+export const CERTBOT_CONTAINER_WEBROOT = `${CERTBOT_CONTAINER_ROOT}/webroot`;
+export const CERTBOT_CONTAINER_CONFIG_DIR = `${CERTBOT_CONTAINER_ROOT}/config`;
+export const CERTBOT_CONTAINER_WORK_DIR = `${CERTBOT_CONTAINER_ROOT}/work`;
+export const CERTBOT_CONTAINER_LOGS_DIR = `${CERTBOT_CONTAINER_ROOT}/logs`;
