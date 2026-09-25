f9676a70df4d15e7beca2df3ce75ec5c9302f6cd
Fabio Gartenmann <137318798+artiphishle@users.noreply.github.com>
2026-09-25 03:30:48 +0200

    fix: bind streamed CLI output

--- src/cli/commands/renew.ts
+++ src/cli/commands/renew.ts
@@ -13,8 +13,8 @@ export const renew: AnkhCommandHandler = async (request) => {
       preference: runtimeOptions.runtimePreference,
       storageDirectory: runtimeOptions.storageDirectory,
       output: {
-        onStdout: request.context.writeStdout,
-        onStderr: request.context.writeStderr,
+        onStdout: (chunk) => request.context.writeStdout(chunk),
+        onStderr: (chunk) => request.context.writeStderr(chunk),
       },
     });
     request.context.writeStdout(
